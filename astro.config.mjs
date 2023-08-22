f4bfc3e0257355d7891f0a0f926139a3a35109a2
safer07 <b0p0halex@gmail.com>
2023-08-22 17:58:05 +0300

    update links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://safer07.github.io",
+  base: "/wargame",
   outDir: "./docs",
 });
+
+// это настройка только для Git pages. нужно удалять параметр site и base, чтобы ссылки нормально работали
